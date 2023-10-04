696e4a730d4d627fd93dc905b1ecd9655a749280
rochelle499 <146861178+rochelle499@users.noreply.github.com>
2023-10-04 16:05:12 +0000

    datatypes_commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use string"; //treat all js code as new version
+
+//alert("3+3") // we are using nodejs not browser
+
+//datatypes
+//number-- 2 to power 53
+//bigint----used for bignumbers
+//string---" "
+//boolean --- true or false
+//undefined----when value is not assigned
+//null----empty standalone value
+//symbol---used for unique
+//object
+
+console.log(typeof("rochelle"))
+//type of null is object but of undefined is undefined
+
+
+console.log("rochelle")
