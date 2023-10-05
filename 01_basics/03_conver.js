f869f6c386e94c600fd36d39eb6745c9c5f91170
rochelle499 <146861178+rochelle499@users.noreply.github.com>
2023-10-05 16:31:30 +0000

    operation_commit

---
+++ 01_basics/03_conver.js
@@ -0,0 +1,60 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************Operations********
+
+// let value=3
+
+// let negvalue= -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+
+// let str1="hello"
+// let str2=" rochelle"
+
+// let str3= str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1"+"2")
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+
+
+// console.log(+true)
+// console.log(+"")
