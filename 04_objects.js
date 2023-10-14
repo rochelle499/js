19b8675d1f25d2f3e9aeafdbf5a10bd387db01d7
rochelle499 <146861178+rochelle499@users.noreply.github.com>
2023-10-14 13:49:47 +0000

    commit

--- 04_objects.js
+++ 04_objects.js
@@ -33,33 +33,62 @@ const obj2={3:'c',4:'d'}
 
 // const obj4=Object.assign({},obj1,obj2)
 
-const obj4={...obj1,...obj2}
+// const obj4={...obj1,...obj2}
 
-console.log(obj4)
+// console.log(obj4)
+
+// console.log(users[1].id)
+// // console.log(tinderUser);
+
+// // console.log(Object.keys(tinderUser));
+// // console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderuser));
+
+// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
-const users = [
-    {
-        id: 1,
-        email: "h@gmail.com"
-    },
-    {
-        id: 12,
-        email: "h@gmail.com"
-    },
-    {
-        id: 15,
-        email: "h@gmail.com"
-    },
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 12,
+//         email: "h@gmail.com"
+//     },
+//     {
+//         id: 15,
+//         email: "h@gmail.com"
+//     },
+// ]
+
+
+//////////////////destructing objectss
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+//destructing an object
+console.log(course.courseInstructor)
+
+
+// const {courseInstructor: instructor} = course
+
+// // console.log(courseInstructor);
+// console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
 ]
-
-console.log(users[1].id)
-// console.log(tinderUser);
-
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderuser));
-
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
-
 
