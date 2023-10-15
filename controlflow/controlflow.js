5e545ac4831a0c8c4561924b9b827b1cf1914900
rochelle499 <146861178+rochelle499@users.noreply.github.com>
2023-10-15 14:24:36 +0000

    commit

---
+++ controlflow/controlflow.js
@@ -0,0 +1,69 @@
+//if 
+const isloggedIn=true
+const temp =41
+
+// if (2=="2"){
+//   console.log('executed')
+// }
+
+// when you have to check for the type of the datatype use  ===
+
+
+// // if else
+// if (temp < 50){
+//   console.log('temp less than 50')
+// }else{
+//   console.log('temp greater than 50')
+// }
+
+//about scope
+// you get error when inside scope const is used
+// both statemnts are executed when var is used
+// const score=200
+
+// if (score <=200){
+//   const power ='fly'
+//   console.log(`User power ${power}`)
+  
+// // }
+// console.log(`User power ${power}`)
+
+
+
+//shorthand for if
+//here scope is implicit
+// multiple statements can be written in one line by using ,
+
+const balance=1000
+
+// if (balance > 500) console.log('test'),
+//   console.log('test2');
+
+
+// if (balance < 500){
+//   console.log('balance is less than 500')
+// }else if(balance < 750){
+//   console.log('balance is less than 750')
+
+  
+// }else if(balance < 1000){
+//   console.log(`balance is less than 1000`)
+// }else{
+//   console.log('balance is greater than 1200')
+// }
+
+
+// and or operator
+
+// const userloggedin= true
+// const debitcard= true
+// const userloggedfromemail=false
+// const userloggedfromgoogle=false
+
+// if (userloggedin && debitcard){
+//   console.log('allow access')
+// }
+
+// if (userloggedfromemail ||userloggedfromgoogle){
+//   console.log('allow access')
+// }
