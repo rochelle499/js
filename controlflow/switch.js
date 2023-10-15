913018d85310e148151d571cb021f2dd80370b8c
rochelle499 <146861178+rochelle499@users.noreply.github.com>
2023-10-15 14:43:09 +0000

    switchcommit

---
+++ controlflow/switch.js
@@ -0,0 +1,47 @@
+// switch (key){
+//     casevalue:
+//     break
+
+//   default:
+//     break;
+// }
+
+
+//it is necessary to write break otherwise it will execute the next statement as well
+
+// const month=3
+// switch (month){
+//   case 1:
+//     console.log("January")
+//     break;
+//   case 2:
+//       console.log("February")
+//       break;
+//     case 3:
+//       console.log("March")
+//       break;
+    
+
+//   default:
+//     console.log("Enter a month")
+//     break;
+// }
+
+
+const month = "jan"
+switch (month){
+  case "jan":
+    console.log("January")
+    break;
+  case "feb":
+      console.log("February")
+      break;
+  case "march":
+      console.log("March")
+      break;
+
+
+  default:
+    console.log("Enter a month")
+    break;
+}
